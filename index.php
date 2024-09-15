422be86de6f4cf397f3d9f4e9487386f2d72e724
JakiaSriRahmawati <jakiasrirahmawati@gmail.com>
2024-09-15 11:59:20 +0700

    menjalankan input data ke mysql

--- index.php
+++ index.php
@@ -1,29 +1,42 @@
 <?php
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Ambil nilai dari input form
-    // $name = $_POST['name'];
-    // $email = $_POST['email'];
-    // $phone = $_POST['phone'];
-    // $address = $_POST['address'];
-    // $country = $_POST['country'];
-    // $card = $_POST['card'];
-    // $security = $_POST['security'];
-    // $name_card = $_POST['name_card'];
-    // $buy_it = $_POST['buy_it'];
+require('koneksi.php');
 
-    // Lakukan validasi atau proses lainnya
-//     echo "Data berhasil dikirim!<br>";
-//     echo "Nama: $name<br>";
-//     echo "Email: $email<br>";
-//     echo "Telepon: $phone<br>";
-//     echo "Alamat: $address<br>";
-//     echo "Negara: $country<br>";
-//     echo "Nomor Kartu: $card<br>";
-//     echo "Kode Keamanan: $security<br>";
-//     echo "Nama di Kartu: $name_card<br>";
-// }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['nama'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $country = $_POST['card'];
+    $card = $_POST['card_type'];
+    $security = $_POST['security_code'];
+    $name_card = $_POST['name_on_card'];
+
+    if (empty($name)) {
+        echo "Nama harus diisi";
+    } elseif (empty($email)) {
+        echo "Email harus diisi";
+    } elseif (empty($phone)) {
+        echo "Telepon harus diisi";
+    } elseif (empty($address)) {
+        echo "Alamat harus diisi";
+    } elseif (empty($country)) {
+        echo "Negara harus diisi";
+    } elseif (empty($card)) {
+        echo "Tipe kartu harus diisi";
+    } elseif (empty($security)) {
+        echo "Kode keamanan harus diisi";
+    } elseif (empty($name_card)) {
+        echo "Nama di kartu harus diisi";
+    } else {
+        $stmt = $conn->prepare("INSERT INTO user (name,email,phone,address,country,card,security,name_card) VALUES(?,?,?,?,?,?,?,?)");
+        $stmt->bind_param('sssssiis', $name, $email, $phone, $address, $country, $card, $security, $name_card);
+        $stmt->execute();
+            header('Location: tabel.php');
+        exit;
+    }
+}
+
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -39,71 +52,62 @@
     </style>    
 </head>
 <body>
-    <fieldset>
-        <legend>Your Details</legend>
-    <form action="" method="POST">
-        <ul>
-                <p>1. <label for="nama">Name :</label>
+    <form method="POST">
+        <fieldset>
+            <legend>Your Details</legend>
+            <ul>
+                <p>1.<label for="nama">Name :</label>
                 <input type="text" id="nama" name="nama" placeholder="Enter your name" required>
                 <p>2.<label for="email">Email :</label>
                 <input type="email" id="email" name="email" placeholder="Enter your email" required>
                 <p>3.<label for="phone">Phone :</label>
-                <input type="number" id="phone" name="phone" placeholder="Enter your phone ">
-        </ul>
-    </form>
-    </fieldset>
-    <fieldset>
-    <legend>Delivery address</legend>
-    <form action="" method="post">
-        <ul>
+                <input type="number" id="phone" name="phone" placeholder="Enter your phone " required>
+            </ul>
+        </fieldset>
+        <fieldset>
+            <legend>Delivery address</legend>
+            <ul>
                 <p>1.<label for="address">Address :</label>
-                <textarea type="text" id="nama" name="address" placeholder="Enter your address"></textarea>
+                <textarea type="text" id="address" name="address" placeholder="Enter your address" required></textarea>
                 <p>2.<label for="post-code">Post code :</label>
-                <input type="text" id="post-code" name="post-code" placeholder="Enter your post code">
+                <input type="text" id="post-code" name="post-code" placeholder="Enter your post code" required>
                 <p>3.<label for="country">Country :</label>
-                <select name="card" id="card">
+                <select name="card" id="card" required>
                 <option value="indonesia">Indonesia</option>
                 <option value="india">India</option>
                 <option value="irlandia">Irlandia</option>
                 </select>
-        </ul>
-    </form>
-    </fieldset>
-    <fieldset>
-    <legend>Card Details</legend>
-    <form action="" method="post">
-        <ul>
-        <p>1.<label for="card" >Card type:</label>
-        <div>
-            <input type="radio" id="visa" name="card_type" value="visa">
-            <label for="visa" required>VISA</label>
-        </div>
-        <div>
-            <input type="radio" id="amex" name="card_type" value="amex">
-            <label for="amex">AmEx</label>
-        </div>
-        <div>
-            <input type="radio" id="mastercard" name="card_type" value="mastercard">
-            <label for="mastercard">Mastercard</label>
-        </div>
-                <p>2.<label for="card number">Card number :</label>
-                <input type="number" id="card number" name="card number" placeholder="Enter your card number" >
-                <p>3.<label for="security code">Security code :</label>
-                <input type="number" id="security code" name="security code" placeholder="Enter your security code " >
-                <p>4.<label for="name on card">Name on card :</label>
-                <input type="text" id="name on card" name="name on card" placeholder="Enter your name on card " >
-        </ul>
+            </ul>
         </fieldset>
-        </form>
         <fieldset>
-            <button type="submit">Buy it!!</button>
+            <legend>Card Details</legend>
+            <ul>
+                <p>1.<label for="card" >Card type:</label>
+                <div>
+                    <input type="radio" id="visa" name="card_type" value="visa" required>
+                    <label for="visa">VISA</label>
+                </div>
+                <div>
+                    <input type="radio" id="amex" name="card_type" value="amex">
+                    <label for="amex">AmEx</label>
+                </div>
+                <div>
+                    <input type="radio" id="mastercard" name="card_type" value="mastercard">
+                    <label for="mastercard">Mastercard</label>
+                </div>
+                <p>2.<label for="card number">Card number :</label>
+                <input type="number" id="card number" name="card number" placeholder="Enter your card number" required>
+                <p>3.<label for="security code">Security code :</label>
+                <input type="number" id="security_code" name="security code" placeholder="Enter your security code " required>
+                <p>4.<label for="name on card">Name on card :</label>
+                <input type="text" id="name on card" name="name_on_card" placeholder="Enter your name on card " required>
+            </ul>
         </fieldset>
-        <script>
-    function redirectAfterSubmit() {
-        setTimeout(function() {
-            window.location.href = 'tabel.php';
-        }, 500); // Redirect setelah 500ms
-    }
-</script>
+        <fieldset>
+        <button type="submit">Buy it!!</button>
+        <button type="reset" onclick="window.history.back();">Close</button>    
+        </fieldset>
+    </form>
+
 </body>
 </html>
