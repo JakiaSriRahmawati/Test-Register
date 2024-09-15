7bcd9cccae7842c1a8b0da394a15c175be468da9
JakiaSriRahmawati <jakiasrirahmawati@gmail.com>
2024-09-15 12:00:58 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,12 @@
 <?php
 $servername = "localhost";
-$database = "db_register";
-$name = "root";
-$email = "";
-$phone = "";
+$username = "root";
+$password = "";
+$database = "db_register1";
 
-$conn = mysqli_connect($servername, $database, $name, $email, $phone);
+$conn = new mysqli($servername,$username,$password,$database);
 if (!$conn){
     die("koneksi Gagal :". mysqli_connect_error());
-}else{
-    echo "Koneksi berhasil";
 }
-// $koneksi = mysqli_connect("localhost", "root", "", "db_registrasi");
+
 ?>
